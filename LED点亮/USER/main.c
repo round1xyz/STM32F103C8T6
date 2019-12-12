bf55646f08626773cfdb2f178d8082fc2d40d282
round1xyz <support@mail.round1.xyz>
2019-12-12 17:39:03 +0800

    LED点亮

---
+++ LED点亮/USER/main.c
@@ -0,0 +1,14 @@
+/**********************************************
+ * 文件名  ：main.c
+ * 描述    ：板子上的LED点亮。
+ * 实验平台：MINI STM32开发板 基于STM32F103C8T6
+ * 库版本  ：ST3.5.0
+***********************************************/
+#include "stm32f10x.h"
+#include "led.h"
+
+int main(void) {
+    SystemInit(); //配置系统时钟为72M
+    LED_GPIO_Config(); //LED端口初始化
+    LED(LED_ON);
+}
